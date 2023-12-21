2263aaadd590213d185ec68fb4644aec65878fb1
Hsk <149724465+Hskdashing@users.noreply.github.com>
2023-12-21 15:29:32 +0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -24,5 +24,5 @@ def get_current_time():
 
 
 def shorten_url(url):
-    site_url = f"https://urlshortx.com/api?api=eda75f630199d1b68143d74c5551605d9eba38dc&url={url}&format=text"
+    site_url = f"https://vnshortener.com/api?api=7286704e3b823d903e91e88a3f6e1b5f7232629a&url={url}&format=text"
     return str(requests.get(site_url).text)
